b24a29376c3f42898930d7ec2b1e9d65b891e780
lintangsutawika <lsutawik@andrew.cmu.edu>
2025-10-31 15:56:04 -0400

    add training scripts

---
+++ scripts/run_training.sh
@@ -0,0 +1,86 @@
+. .env
+
+while getopts ":m:n:d:s:" opt; do
+  case ${opt} in
+    m ) MODEL=$OPTARG;;
+    n ) N_ROLLOUTS=$OPTARG;;
+    d ) DATA_PATH=$OPTARG;;
+    s ) CKPT_PATH=$OPTARG;;
+    # \? ) echo "Usage: cmd [-u] [-p]";;
+  esac
+done
+
+MODEL_ALIAS=$(echo $MODEL | sed 's/\//-/g')
+# Get number of GPUs available
+NUM_GPUS=$(nvidia-smi -L | wc -l)
+N_ROLLOUTS="${N_ROLLOUTS:-4}"
+MAX_LENGTH=8192
+RUN_NAME="code_search_${MODEL_ALIAS}"
+set -x
+
+# Colocated GRPO training+generation for Qwen3-8B on the SWE-Bench task.
+# Uses 1 node with 8 GPUs.
+# uv run --isolated examples/mini_swe_agent/preprocess_swegym.py --output_dir ~/data/swe_gym_subset
+# bash examples/mini_swe_agent/run_mini_swe_8B.sh
+
+# Save trajectories here for debugging
+# NOTE: For a multi-node cluster, ensure that this is on NFS so that you can save all trajectories in the same path
+
+# DATA_DIR="data/swe_smith"
+DATA_PATH="${DATA_PATH:-data/swe_smith}"
+CKPT_PATH="${CKPT_PATH:-ckpts/${MODEL_ALIAS}}"
+
+NNODES=1
+NUM_INFERENCE_ENGINES=4
+TP_SIZE=1
+LOGGER=wandb
+
+# We use a small batch size here for demonstration
+# NOTE (sumanthrh): The `generator.max_turns` here is actually unused, and we use the `step_limit` from the `swebench.yaml` file. 
+uv run --isolated -m rca.train \
+  data.train_data="['$DATA_PATH/train.parquet']" \
+  data.val_data="['$DATA_PATH/validation.parquet']" \
+  trainer.algorithm.advantage_estimator="grpo" \
+  trainer.policy.model.path=${MODEL} \
+  trainer.placement.colocate_all=true \
+  trainer.strategy=fsdp2 \
+  trainer.placement.policy_num_gpus_per_node=$NUM_GPUS \
+  trainer.placement.ref_num_gpus_per_node=$NUM_GPUS \
+  trainer.placement.policy_num_nodes=$NNODES \
+  trainer.placement.ref_num_nodes=$NNODES \
+  trainer.policy.sequence_parallel_size=$NUM_GPUS \
+  generator.num_inference_engines=$NUM_INFERENCE_ENGINES \
+  generator.inference_engine_tensor_parallel_size=$TP_SIZE \
+  +generator.traj_dir=$CKPT_PATH/trajectories/ \
+  trainer.epochs=20 \
+  trainer.eval_batch_size=50 \
+  trainer.eval_before_train=false \
+  trainer.eval_interval=5 \
+  trainer.update_epochs_per_batch=1 \
+  trainer.train_batch_size=8 \
+  trainer.policy_mini_batch_size=8 \
+  trainer.micro_forward_batch_size_per_gpu=4 \
+  trainer.micro_train_batch_size_per_gpu=2 \
+  trainer.dump_data_batch=true \
+  trainer.ckpt_interval=10 \
+  trainer.max_prompt_length=4096 \
+  generator.sampling_params.max_generate_length=${MAX_LENGTH} \
+  generator.max_input_length=30720 \
+  generator.max_turns=20 \
+  trainer.policy.optimizer_config.lr=1.0e-6 \
+  trainer.algorithm.use_kl_loss=true \
+  generator.backend=vllm \
+  generator.run_engines_locally=True \
+  generator.enable_http_endpoint=True \
+  generator.http_endpoint_host='0.0.0.0' \
+  generator.http_endpoint_port=8080 \
+  generator.weight_sync_backend=nccl \
+  generator.async_engine=true \
+  generator.batched=true \
+  generator.n_samples_per_prompt=${N_ROLLOUTS} \
+  generator.gpu_memory_utilization=0.8 \
+  trainer.logger="$LOGGER" \
+  trainer.project_name="rca" \
+  trainer.run_name=${RUN_NAME} \
+  trainer.resume_mode=null \
+  trainer.ckpt_path="$CKPT_PATH"
