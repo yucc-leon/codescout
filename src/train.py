5784ac799586c157ea5e8874b7bc55c4563ec0b9
lintangsutawika <lsutawik@andrew.cmu.edu>
2025-11-15 04:57:56 +0000

    choose between async and sync training

--- src/train.py
+++ src/train.py
@@ -4,7 +4,10 @@ from skyrl_train.entrypoints.main_base import BasePPOExp, config_dir, validate_c
 from skyrl_train.utils import initialize_ray
 import ray
 
+import asyncio
+
 from src.generator.code_search_generator import CodeSearchGenerator
+from src.async_trainer import AsyncRayPPOTrainer
 
 
 class CodeSearchPPOExp(BasePPOExp):
@@ -18,11 +21,42 @@ class CodeSearchPPOExp(BasePPOExp):
         )
         return generator
 
+class AsyncCodeSearchPPOExp(CodeSearchPPOExp):
+    def get_trainer(
+        self,
+        cfg,
+        tracker,
+        tokenizer,
+        train_dataset,
+        eval_dataset,
+        inference_engine_client,
+        generator,
+        colocate_pg,
+    ):
+        return AsyncRayPPOTrainer(
+            cfg=cfg,
+            tracker=tracker,
+            tokenizer=tokenizer,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            inference_engine_client=inference_engine_client,
+            generator=generator,
+            colocate_pg=colocate_pg,
+        )
+
+    def run(self):
+        trainer = self._setup_trainer()
+        # Start the async training loop
+        asyncio.run(trainer.train())
+
 
 @ray.remote(num_cpus=1)
 def skyrl_entrypoint(cfg: DictConfig):
     # make sure that the training loop is not run on the head node.
-    exp = CodeSearchPPOExp(cfg)
+    if cfg.get("run_async_trainer", False):
+        exp = AsyncCodeSearchPPOExp(cfg)
+    else:
+        exp = CodeSearchPPOExp(cfg)
     exp.run()
 
 
